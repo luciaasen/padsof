374ff1b6b8a1815ea80c810237881e483026f7ad
juan260 <superjuan260@hotmail.com>
2017-03-23 00:12:25 +0100

    aojfgns

--- p3/src/moon/AcademyTest.java
+++ p3/src/moon/AcademyTest.java
@@ -13,9 +13,11 @@ import org.junit.After;
 public class AcademyTest {
 
 	@Before
-	public void setUp(){
+	public void setUp() throws NumberFormatException, IOException{
 		Academy moon = Academy.getMoonApp();
+		moon.loadUsers("StudentData.txt");
 		
+		moon.setTeacher(t)	
 	}
 	
 	
