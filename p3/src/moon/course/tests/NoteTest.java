f45bddf23b4cebd1bb2d2d76ec8211ecb84ead65
luciaasen <luciaasen@gmail.com>
2017-03-23 01:04:21 +0100

    Muchas estructuras de junits añadidas

---
+++ p3/src/moon/course/tests/NoteTest.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ */
+package moon.course.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author lucia
+ *
+ */
+public class NoteTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link moon.course.Note#getText()}.
+	 */
+	@Test
+	public void testGetText() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link moon.course.Note#setText(java.lang.String)}.
+	 */
+	@Test
+	public void testSetText() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link moon.course.CourseElement#getCourse()}.
+	 */
+	@Test
+	public void testGetCourse() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link moon.course.CourseElement#getUnit()}.
+	 */
+	@Test
+	public void testGetUnit() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link moon.course.CourseElement#makeVisible()}.
+	 */
+	@Test
+	public void testMakeVisible() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link moon.course.CourseElement#makeInvisible()}.
+	 */
+	@Test
+	public void testMakeInvisible() {
+		fail("Not yet implemented");
+	}
+
+}
