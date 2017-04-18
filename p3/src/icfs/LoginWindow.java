a1cc216b3b3e8ac73260a576deaf35514852ef82
juan260 <superjuan260@hotmail.com>
2017-04-18 18:48:20 +0200

    as

--- p3/src/icfs/LoginWindow.java
+++ p3/src/icfs/LoginWindow.java
@@ -5,36 +5,72 @@ import java.awt.*;
 import javax.swing.*;
 
 
-public class LoginWindow {
+public class LoginWindow extends JPanel {
 	
+	
+	private static final long serialVersionUID = 1L;
+
 	/*public static void main(String args[]){
 		login();
 	}*/
-	public static void main(String args[]) {
+	public LoginWindow() {
+		
+		JPanel buttons = buttons();
+		JPanel text = text();
+		
+		this.setSize(400, 400);
+		this.setVisible(true);
+		this.setLayout(new BorderLayout());
+		this.add(buttons, BorderLayout.SOUTH);
 
-		JFrame w1n = new JFrame("Gui");
+		this.add(text, BorderLayout.CENTER);
+	}
+	
+	private JPanel buttons(){
+		JPanel buttons = new JPanel();
 		JButton l0g1nB = new JButton("Login");
 		JButton c4nc3lB = new JButton("Cancel");
+		buttons.add(l0g1nB);
+		buttons.add(c4nc3lB);
+		buttons.setVisible(true);
+		
+		buttons.setBackground(Color.cyan);
+		
+		return buttons;
+	}
+	
+	public JPanel text(){
+		SpringLayout layout = new SpringLayout();
+		JPanel text = new JPanel();
 		JTextField emilioText = new JTextField(10);
 		JPasswordField password = new JPasswordField(10);
-		Container container = w1n.getContentPane();
-		container.setLayout(new FlowLayout());
-		emilioText.setSize(500, 500);
+		JLabel emailLabel = new JLabel("emailLabel");
+		JLabel passwordLabel = new JLabel("passwordLabel");
+		text.setLayout(layout);
+		emilioText.setSize(100, 100);
+		
+		/* Email label constraints */
+		layout.putConstraint(SpringLayout.WEST, emailLabel, 5, SpringLayout.WEST, text);
+		layout.putConstraint(SpringLayout.NORTH, emailLabel, 5, SpringLayout.NORTH, text);
+		layout.putConstraint(SpringLayout.EAST, emailLabel, 5, SpringLayout.WEST, emilioText);
+		
+		/* Email text constraints */
+		layout.putConstraint(SpringLayout.NORTH, emailLabel, 0, SpringLayout.NORTH, emilioText);
+		layout.putConstraint(SpringLayout.EAST, emilioText, 5, SpringLayout.EAST, text);
+		
+		/* Password label constraints */
+		layout.putConstraint(SpringLayout.WEST, passwordLabel, 0, SpringLayout.WEST, emailLabel);
+		layout.putConstraint(SpringLayout.NORTH, passwordLabel, 5, SpringLayout.SOUTH, emailLabel);
+		layout.putConstraint(SpringLayout.EAST, passwordLabel, 5, SpringLayout.WEST, password);
 		
 		
-		container.add(emilioText);
-		container.add(password);
-		container.add(l0g1nB);
-		container.add(c4nc3lB);
-		container.setVisible(true);
+		text.add(emilioText);
+		text.add(password);
+		text.setVisible(true);
 		
-		container.setBackground(Color.cyan);
+		text.setBackground(Color.RED);
 		
-		
-		password.setSize(100, password.getHeight());
-		w1n.setSize(1000, 140);
-		w1n.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		w1n.setVisible(true);
+		return text;
 	}
 	
 }
