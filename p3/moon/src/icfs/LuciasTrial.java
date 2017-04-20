e880d5b42b82954fd837496244a7a510615c9cba
Lucia Asencio Martin <luciaasen@gmail.com>
2017-04-20 20:10:40 +0200

    hi

---
+++ p3/moon/src/icfs/LuciasTrial.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package icfs;
+
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * @author lusilu
+ *
+ */
+public class LuciasTrial {
+	JFrame loginWindow = new JFrame("moon");
+	Container cont = loginWindow.getContentPane();
+	cont.setLayout(new BorderLayout());
+}
